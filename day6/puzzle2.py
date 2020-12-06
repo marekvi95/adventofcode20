858e7b06ccebcaca6a959cc93127512b760b4614
Marek Vitula <mvitula@domaci-pc.lan>
2020-12-06 14:26:05 +0100

    Improved solution to problem 2

--- day6/puzzle2.py
+++ day6/puzzle2.py
@@ -1,42 +1,9 @@
-from string import ascii_lowercase
+import os
 
-filename = "C:\\Users\\nxf46245\\Documents\\adventofcode20\\day6\\input.txt"
+filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt")
+an_sum = 0
 
-with open(filename) as f:
-    content = f.readlines()
-content = [x.strip() for x in content]
+for group in open(filename).read().split('\n\n'):
+    an_sum += len(set.intersection(*map(set, group.split('\n'))))
 
-questions = list()
-question = list()
-questions_uniq = list()
-
-for x in content:
-    if len(x) == 0:
-        questions.append(question)
-        question = list()
-    else:
-        question.append(x)
-
-questions.append(question)
-print(questions)
-
-sum = 0
-cnt = 0
-
-
-for x in questions:
-    for c in ascii_lowercase:    
-        # print(range(len(x)), x, c)
-        for i in range(len(x)):
-            if c in x[i]:
-                cnt += 1
-                if cnt == len(x):
-                    # print("tada", cnt, x[i], c)
-                    sum += 1
-                    cnt = 0
-            else:
-                cnt = 0
-
-
-print(sum)
-
+print(an_sum)
